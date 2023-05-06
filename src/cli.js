9d3c0c6ed116e4960978aacef7e8cf2b54592b69
ismailibnaslanbek <isma.boss93@mail.ru>
2023-05-06 22:47:59 +0300

    game

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const greeting = () => {
-  const userName = readlineSync.question('May i have your name? ');
-  console.log(`Hello ${userName}!`);
-};
-export default greeting;
