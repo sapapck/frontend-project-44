7c2d11df571795050068899e3fe2dc51e6be8913
ismailibnaslanbek <isma.boss93@mail.ru>
2023-05-06 22:50:53 +0300

    game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,4 +1,5 @@
-import { gameArchitecture, getRandomData } from '../index.js';
+import gameArchitecture from '../index.js';
+import getRandomData from '../utils.js';
 
 const rule = 'What number is missing in the progression?';
 
@@ -7,21 +8,16 @@ const createProgression = () => {
   const step = getRandomData(1, 10);
 
   const progression = [getRandomData(1, 20)];
-
   while (progression.length < progressionLength) {
     const lastIndex = progression.length - 1;
     progression.push(progression[lastIndex] + step);
   }
-  return progression;
-};
-
-const task = () => {
-  const progression = createProgression();
   const missedNumber = getRandomData(1, progression.length - 1);
   const correctAnswear = `${progression[missedNumber]}`;
   progression[missedNumber] = '..';
   const question = progression.join(' ');
   return [question, correctAnswear];
 };
-const gameProgression = () => gameArchitecture(rule, task);
+
+const gameProgression = () => gameArchitecture(rule, createProgression);
 export default gameProgression;
