5d5340b7640bc7a8dd45924b88393ef6ed00360a
ismailibnaslanbek <isma.boss93@mail.ru>
2023-05-06 22:50:16 +0300

    game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,19 +1,20 @@
-import { gameArchitecture, getRandomData } from '../index.js';
+import gameArchitecture from '../index.js';
+import getRandomData from '../utils.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
-const greatestCommonDivisor = (a, b) => {
+const getGcd = (a, b) => {
   if (!b && b === 0) {
     return a;
   }
-  return greatestCommonDivisor(b, a % b);
+  return getGcd(b, a % b);
 };
 
 const randomNum = () => {
   const num1 = getRandomData(1, 50);
   const num2 = getRandomData(1, 50);
   const question = `${num1} ${num2}`;
-  const correctAnswer = `${greatestCommonDivisor(num1, num2)}`;
+  const correctAnswer = `${getGcd(num1, num2)}`;
   return [question, correctAnswer];
 };
 
