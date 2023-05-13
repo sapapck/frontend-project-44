706b7c59c12c1129490774be4c9e68361f47167d
ismailibnaslanbek <isma.boss93@mail.ru>
2023-05-13 21:31:20 +0300

    game

--- src/games/even.js
+++ src/games/even.js
@@ -2,13 +2,13 @@ import getRandomData from '../utils.js';
 import gameArchitecture from '../index.js';
 
 const isEven = (number) => number % 2 === 0;
-const rule = 'Answer "yes" if the number is even, otherwise answer "no"';
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 
 const getQuestionAndAnswer = () => {
   const question = getRandomData(1, 99);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
-const gameEven = () => gameArchitecture(rule, getQuestionAndAnswer);
+const gameEven = () => gameArchitecture(description, getQuestionAndAnswer);
 
 export default gameEven;
